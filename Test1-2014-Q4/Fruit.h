cd109147911f8f9e14c24c8667e56d44025f3210
randalx2 <pmisthry@gmail.com>
2016-03-27 20:39:05 +0200

    Created class declarations

--- Test1-2014-Q4/Fruit.h
+++ Test1-2014-Q4/Fruit.h
@@ -7,11 +7,37 @@ using namespace std;
 
 class Fruit
 {
-protected:
+protected: string nameFruit, colourFruit;
 
 public:
 	Fruit();
 	~Fruit();
+	virtual void name() = 0;
+	virtual void colour() = 0;
+};
+
+class Apple : public Fruit
+{
+public: Apple();
+		~Apple();
+		void name();
+		void colour();
+};
+
+class Orange : public Fruit
+{
+public: Orange();
+		Orange();
+		void name();
+		void colour();
+};
+
+class Banana : public Fruit
+{
+public: Banana();
+		Banana();
+		void name();
+		void colour();
 };
 
 #endif
